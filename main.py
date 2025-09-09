35b64b059bdb285550014bb1124734e1ab5fc915
peggy-lee-zorba <peggy.lee.zorba.official@gmail.com>
2025-09-09 20:25:32 +0300

    main edit

--- main.py
+++ main.py
@@ -22,4 +22,5 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=os.getenv('FLASK_DEBUG', False))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=os.getenv('FLASK_DEBUG', False))
